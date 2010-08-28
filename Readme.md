a2a8817a1d578995b79b82da988b217e3bd9b4e7
Brian Egan <brian@brianegan.com>
2010-08-27 19:56:55 -0700

    Ok, removing nDistro packages b/c it doesn't support some libs. Moving to npm b/c we're on joyent

--- Readme.md
+++ Readme.md
@@ -1,2 +1,3 @@
 # Super awesome secret project. More to come.
 
+Ok, ok... we're working on a super badass music player.
--- server.js
+++ server.js
@@ -1,11 +1,1 @@
-var http = require('http'),
-    cp = require('child_process'),
-    port = parseInt(process.env.PORT) || 8001 
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'})
-  cp.exec("node -v", function(error, stdout, stderr) {
-    res.end(stdout);
-  });
-  
-}).listen(port)
+var r = require('express')
