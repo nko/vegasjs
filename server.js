fad1eb68f79d41a144dfb9577eb240033f2faef7
Brian Egan <brian@brianegan.com>
2010-08-29 15:05:43 -0700

    Updating the server.js so that it's a bit more commented and includes the ability to get a JSON response of the catalog

--- server.js
+++ server.js
@@ -1,22 +1,36 @@
-var app = require('express').createServer(),
+/*!
+ * nTunes
+ * Copyright(c) 2010 Brian Egan <brian@brianegan.com> and Dylan Bathurst <Dylan Bathurst <dylanbathurst@gmail.com>
+ * MIT Licensed
+ */
+    
+    
+var // Module Deps
+    app = require('express').createServer(),
     express = require('express'),
-    sys = require('sys'),
     couchdb = require('couchdb'),
+    
+    // CouchDB Connection
     client = couchdb.createClient(5984, 'vegasjs.couchone.com'),
-    db = client.db('ntunes');
-var self = this;
+    db = client.db('ntunes'),
+    
+    // Save this from itself!
+    self = this;
+    
 this.local = [];
 
-app.use(express.staticProvider(__dirname + '/public'));
+/**
+ * Express config
+ */
+app.use(express.staticProvider(__dirname + '/public')); // Serve static files from /public
+app.set('views', __dirname + '/views'); // Use /views as our template directory
 
-db.getDoc('ca988e74ba39dd1c6431d61f01001624', function(er, doc) {
-  if (er) throw new Error(JSON.stringify(er));
-  sys.puts('Fetched my new doc from couch:');
-  sys.puts(sys.inspect(doc));
-  self.local.push(doc);
-});
 
-app.set('views', __dirname + '/views');
+/**
+ * Routes
+ */
+ 
+// Home Page
 app.get('/', function(req, res){
   res.render('index.ejs', {
     locals: {
@@ -25,17 +39,38 @@ app.get('/', function(req, res){
   });
 });
 
-app.listen(80);
-// 
-// db
-//   .allDocs(function(er, doc){
-//     if (er) throw new Error(JSON.stringify(er));
-//       console.log('Fetched my new doc from couch:');
-//       console.log(doc);
-//   });
-// db
-//   .saveDoc('my-doc', {awesome: 'couch fun'}, function(er, ok) {
-//     if (er) throw new Error(JSON.stringify(er));
-//     sys.puts('Saved my first doc to the couch!');
-//   });
-// 
+// Catalog
+app.get('/catalog', function(req, res) {
+  
+  // Create an object to send as our response
+  var response = {
+    "songs": []
+  };
+  
+  // Grab all our docs from CouchIO
+  db.allDocs(function (err, all) {
+    
+    var records = all.rows, // All the rows in our DB
+        addSong = function(doc) { // The function we use to add Songs to the respons untl the response is ready
+          response.songs.push(doc); // Add a song to our response
+          if (response.songs.length == all.rows.length) { // If we've got all our songs
+            res.send(response); // Send the response!
+          };
+        };
+    
+    // Runs through all the rows (ie Songs), grabbing their data and stuffing it into the response
+    for (var i=0; i < records.length; i++) {
+      db.getDoc(records[i].id, function(docErr, doc) {        
+        if (docErr) {
+          // Homey don't play dat.
+          throw new Error(JSON.stringify(docErr));
+        } else {
+          addSong(doc);
+        }         
+      });
+    };
+  }); // end db.allDocs
+}); // end app.get 
+
+// Listen on port 80
+app.listen(80); 
