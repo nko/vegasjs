25b1e27ff06ce293246d59bb41473bad70961d0a
dbathurst <dbathurst@zappos.com>
2010-08-28 22:56:34 -0700

    fixed js for window resizing

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var app = require('express').createServer(),
     express = require('express'),
     sys = require('sys'),
-    couchdb = require('couchdb'),
+    couchdb = require('node-couchdb/lib/couchdb'),
     client = couchdb.createClient(5984, 'vegasjs.couchone.com'),
     db = client.db('ntunes');
 var self = this;
-this.local;
+this.local = [];
 
 app.use(express.staticProvider(__dirname + '/public'));
 
@@ -13,7 +13,7 @@ db.getDoc('ca988e74ba39dd1c6431d61f01001624', function(er, doc) {
   if (er) throw new Error(JSON.stringify(er));
   sys.puts('Fetched my new doc from couch:');
   sys.puts(sys.inspect(doc));
-  self.local = doc;
+  self.local.push(doc);
 });
 
 app.set('views', __dirname + '/views');
@@ -25,7 +25,7 @@ app.get('/', function(req, res){
   });
 });
 
-app.listen(80);
+app.listen(3000);
 // 
 // db
 //   .allDocs(function(er, doc){
